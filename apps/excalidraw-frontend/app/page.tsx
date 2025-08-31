51a055e62f1d5fea175a31ea21ccde13d846ac3f
Sam <armandawoodkhan@gmail.com>
2025-08-31 19:39:44 +0530

    Landing Page First draft completed

--- apps/excalidraw-frontend/app/page.tsx
+++ apps/excalidraw-frontend/app/page.tsx
@@ -1,36 +1,64 @@
 import { LandingBackground } from "@/components/LandingBackground";
+import { Star } from "lucide-react";
 import Link from "next/link";
-
 export default function Home() {
   return (
-    <main className="relative font-sans text-white">
-      <LandingBackground/>
-      {/* <div className="relative z-10 flex min-h-screen flex-col items-center justify-center p-4">
-                    <div className="max-w-2xl text-center rounded-2xl bg-black/40 p-8 backdrop-blur-md border border-white/10">
-                        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-                            Build Your Frontend
-                        </h1>
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                            This is your foreground content. You can build your entire application here, and the background will remain fixed behind it. The key is using CSS `position` and `z-index` properties correctly.
-                        </p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a href="#" className="rounded-md bg-white px-4 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-gray-200 fs-visible:outlineocu focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
-                                Get started
-                            </a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">
-                                Learn more <span aria-hidden="true">→</span>
-                            </a>
-                        </div>
-                    </div>
-      </div> */}
-      <div className="relative top-4 left-8 text-3xl font-bold font-sans z-10 bg-gradient-to-b from-[#55535310] to-[#A0A0A010] w-fit p-4"><h1>COLLABCANVAS</h1></div>
-      <div  className="absolute z-10 left-32 top-48 text-wrap w-3/9 flex flex-col gap-4 ">
-        <h2 className="text-6xl font-bold leading-snug">VISUALIZE YOUR WORK</h2>
-        <div>
-          <h3 className="text-2xl font-semibold">Unify Your Team's Vision.</h3>
-          <p className="text-[14px] text-[#DDDDDD]">Collab Canvasprovides a single,unified "canvas" where a team's Entire project lifecycle can unfold visually. Think of it as an infinite digital wall for real-time collaboration..</p>
+    <main className="relative font-sans text-white min-h-screen overflow-hidden">
+      <LandingBackground />
+      
+      <div className="relative top-4 left-8 text-3xl font-bold font-sans z-10 bg-gradient-to-b from-[#55535310] to-[#A0A0A010] w-fit p-4">
+        <h1>COLLABCANVAS</h1>
+      </div>
+      <div className="absolute left-32 top-46 flex justify-between w-[62.5%]">
+        <div className=" z-10 text-wrap w-1/2 flex flex-col gap-4 ">
+          <h2 className="text-6xl font-bold leading-snug font-sans">
+            VISUALIZE YOUR <span className="text-[#9f9f9f]">WORK</span>
+          </h2>
+          <div>
+            <h3 className="text-2xl font-semibold">
+              Unify Your Team's Vision.
+            </h3>
+            <p className="text-[14px] text-[#DDDDDD]">
+              Collab Canvasprovides a single, unified "canvas" where a team's
+              Entire project lifecycle can unfold visually. Think of it as an
+              infinite digital wall for real-time collaboration..
+            </p>
+          </div>
+          <Link
+            href={"/canvas/1"}
+            className="bg-black text-xl font-bold px-6 py-3 rounded-full w-fit text-center "
+          >
+            START COLLABORATING
+          </Link>
         </div>
-        <Link href={'/canvas/1'} className="bg-black text-xl font-bold px-6 py-3 rounded-full w-fit text-center ">START COLLABORATING</Link>
+        <div className="">
+          <img className="relative top-[-50px] -right-[120px] opacity-60 size-[446px]" src="/drawing.svg" alt="drawing" />
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-10"><img  className="size-64" src="./pencil.svg" alt="pencil" /></div>
+      <div className="absolute top-28 right-32 w-fit py-4 px-8  bg-gradient-to-r from-[#55535310] to-[#A0A0A010] flex flex-col justify-center items-center">
+        <div className="flex">
+          <Star fill="#FFFFFF"/>
+          <Star fill="#FFFFFF"/>
+          <Star fill="#FFFFFF"/>
+          <Star fill="#FFFFFF"/>
+          <Star fill="#8D8D8D" stroke="#8D8D8D" />
+        </div>
+        <p>1000+ reviews</p>
+      </div>
+      <div className="display flex absolute bottom-12 right-[100px] w-3/5 gap-4">
+        <div className="w-fit py-4 px-8  bg-gradient-to-r from-[#55535310] to-[#A0A0A010] flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Real-Time Sync</h5>
+            <p className="text-[14px] text-center">Instantaneous, multi-user cursor tracking and updates</p>
+        </div>
+        <div className=" w-fit py-4 px-8  bg-gradient-to-r from-[#55535310] to-[#A0A0A010] flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Canvas Space</h5>
+            <p className="text-[14px] text-center">Infinite, zoomable workspace for boundless ideation.</p>
+        </div>
+        <div className="w-fit py-4 px-8  bg-gradient-to-r from-[#55535310] to-[#A0A0A010] flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Ideal User</h5>
+            <p className="text-[14px] text-center">Agile teams, product managers, and visual thinkers.</p>
+        </div>
       </div>
     </main>
   );
--- apps/excalidraw-frontend/components/LandingBackground.tsx
+++ apps/excalidraw-frontend/components/LandingBackground.tsx
@@ -5,13 +5,14 @@ export const LandingBackground = () => {
       <div className="absolute inset-0 flex justify-between">
           <div className="relative flex w-1/2 justify-between overflow-hidden bg-[#1A1919]">
           <div className="h-screen w-32 bg-gradient-to-b from-[#000000] to-[#181818] opacity-50"></div>
+          <div className="absolute top-0 left-[530px] opacity-50"><img  className="size-64" src="./pencil.svg" alt="pencil" /></div>
           <div className="absolute left-[2rem] -bottom-[19rem] rounded-full size-[32rem] overflow-hidden bg-gradient-to-t from-[#5A5A5A] to-white blur-[16em]  bg-[5A5A5A]"></div>
           
           <div className="h-screen w-64 bg-gradient-to-b from-[#000000] to-[#181818] opacity-52"></div>
         </div>
         <div className="relative h-screen w-1/5 bg-gradient-to-b from-[#000000] to-[#505050] opacity-20">
-          <p className="absolute top-[250px] -left-[85px] font-bold font-sans text-red-600 rotate-270 text-[165px] leading-[134%]text-stroke ">STEATH</p>
-          <p className="absolute top-[16rem] -left-[5rem] font-bold font-sans text-black rotate-270 text-[163.26px] leading-[134%]text-stroke ">STEATH</p>
+          {/* <p className="absolute top-[250px] -left-[85px] font-bold font-sans text-red-600 rotate-270 text-[165px] leading-[134%]text-stroke ">STEATH</p> */}
+          <p className="absolute top-[16rem] -left-[5rem] font-bold font-sans text-black rotate-270 text-shadow-[0 0 3px #FF0000] [text-shadow:_0_0px_8px_#FF0000] text-[163.26px] leading-[134%]text-stroke ">STEATH</p>
         </div>
       </div>
     </div>
